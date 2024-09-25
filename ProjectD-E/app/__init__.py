b79435076c6f910327c39af91b1409118eea20bf
c2p31047 <115206538+c2p31047@users.noreply.github.com>
2024-09-25 21:53:28 +0900

    Add files via upload

---
+++ ProjectD-E/app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_migrate import Migrate
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'your_secret_key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+    app.config['UPLOAD_FOLDER'] = 'uploads'
+
+
+    db.init_app(app)
+    login_manager.init_app(app)
+    login_manager.login_view = 'main.login'
+    migrate.init_app(app, db)
+
+    from .routes import main
+    app.register_blueprint(main)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        from .models import User
+        return User.query.get(int(user_id))
+
+    return app
